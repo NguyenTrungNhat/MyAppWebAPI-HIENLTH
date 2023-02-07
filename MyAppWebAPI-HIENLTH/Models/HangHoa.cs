17df016776574bb1c09126322b95a03b1e497e82
Vinamilk <nhatnguyen20102002@gmail.com>
2023-02-07 23:31:17 +0700

    Add project files.

---
+++ MyAppWebAPI-HIENLTH/Models/HangHoa.cs
@@ -0,0 +1,15 @@
+﻿namespace MyAppWebAPI_HIENLTH.Models
+{
+    public class HangHoaVM
+    {
+        public string TenHangHoa { get; set; }
+        public double DonGia { get; set; }
+
+    }
+
+    public class HangHoa : HangHoaVM
+    {
+        public Guid MaHangHoa { get; set; }
+
+    }
+}
