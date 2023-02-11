aeb9e48787e67594880e182ed3e8273824d73b31
Vinamilk <nhatnguyen20102002@gmail.com>
2023-02-12 00:45:28 +0700

    Lesson 2 - migration

---
+++ MyAppWebAPI-HIENLTH/Models/LoaiModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyAppWebAPI_HIENLTH.Models
+{
+    public class LoaiModel
+    {
+        [Required]
+        [MaxLength(50)]
+        public string TenLoai { get; set; }
+    }
+}
