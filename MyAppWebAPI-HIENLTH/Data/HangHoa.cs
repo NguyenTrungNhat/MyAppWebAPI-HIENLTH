7259e861b5933ddc21627119aa0336de5514a40e
Vinamilk <nhatnguyen20102002@gmail.com>
2023-02-12 23:51:18 +0700

    FluentAPi

--- MyAppWebAPI-HIENLTH/Data/HangHoa.cs
+++ MyAppWebAPI-HIENLTH/Data/HangHoa.cs
@@ -21,6 +21,10 @@ namespace MyAppWebAPI_HIENLTH.Data
 
         public int? MaLoai { get; set; }
         [ForeignKey("MaLoai")]
-        public Loai Loai { get; set; }
+        public ICollection<DonHangChiTiet> DonHangChiTiets { get; set; }
+        public HangHoa()
+        {
+            DonHangChiTiets = new HashSet<DonHangChiTiet>();
+        }
     }
 }
--- MyAppWebAPI-HIENLTH/Data/MyDbContext.cs
+++ MyAppWebAPI-HIENLTH/Data/MyDbContext.cs
@@ -9,6 +9,35 @@ namespace MyAppWebAPI_HIENLTH.Data
         #region DBSet
         public DbSet<HangHoa> HangHoas { get; set; }
         public DbSet<Loai> Loais { get; set; }
+        public DbSet<DonHang> DonHangs { get; set; }
+        public DbSet<DonHangChiTiet> DonHangChiTiets { get; set; }
         #endregion
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<DonHang>(e =>
+            {
+                e.ToTable("DonHang");
+                e.HasKey(dh => dh.MaDh);
+                e.Property(dh => dh.NgayDat).HasDefaultValueSql("getutcdate()");
+                e.Property(dh => dh.NguoiNhan).IsRequired().HasMaxLength(100);
+            });
+
+            modelBuilder.Entity<DonHangChiTiet>(entity =>
+            {
+                entity.ToTable("ChiTietDonHang");
+                entity.HasKey(e => new {e.MaDh,e.MaHh});
+
+                entity.HasOne(e => e.DonHang)
+                .WithMany(e => e.DonHangChiTiets)
+                .HasForeignKey(e => e.MaDh)
+                .HasConstraintName("FK_DOnHangCT_DonHang");
+
+                entity.HasOne(e => e.HangHoa)
+                .WithMany(e => e.DonHangChiTiets)
+                .HasForeignKey(e => e.MaHh)
+                .HasConstraintName("FK_DOnHangCT_HangHoa");
+            });
+        }
+    
     }
 }
